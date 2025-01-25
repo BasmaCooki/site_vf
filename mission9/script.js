602bff2db15728fffe231d9be3930f1413219151
BasmaCooki <basma.guettouche.etudiant@gmail.com>
2025-01-25 23:53:52 +0100

    mission9

---
+++ mission9/script.js
@@ -0,0 +1,13 @@
+function testqcm() {
+    var score = 0;
+
+    if (document.getElementById("q1r3").checked) {
+        score++;
+    }
+    
+    alert("Votre score est de : " + score + "/1");
+}
+
+function ouvrirCorrige() {
+    window.open('corrige.html', '_blank', 'width=600,height=400');
+}
